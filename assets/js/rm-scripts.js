f9f987fbe4b6c3a591eb1b3792ec30df45300f57
Renato Macedo <rmacedo@freead.com.br>
2015-02-25 17:06:55 -0300

    Campo busca animação

---
+++ assets/js/rm-scripts.js
@@ -0,0 +1,40 @@
+/* JS Document
++----------------------------------------------------------------+
+:: Blog / Portfolio - RM Renato Macedo Front-End Developer
+:: Versão: 1.0
+:: Front-End: Renato de Macedo
+:: Data: 05/02/2015
++----------------------------------------------------------------+
+*/
+
+$(document).ready(function(){
+
+/*
++----------------------------------------------------------------+
+:: Search Top Animate
++----------------------------------------------------------------+
+*/
+
+	$(".iconLupa").click(function(e){
+		e.preventDefault();
+		var inputSearch = $(".searchInput");
+		var fieldSearch = $(".fieldSearch");
+
+		
+		inputSearch.toggleClass("openSearch");
+		fieldSearch.show().focus();
+
+		if (fieldSearch.val() != "") {
+			$('#formSearch').submit();
+		};
+
+	});
+
+
+});
+
+
+
+
+
+
